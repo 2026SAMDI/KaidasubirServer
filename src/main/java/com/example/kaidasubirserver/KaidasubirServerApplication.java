43abd273f8b681ef071729a64ac3149f7379766c
powerGumdam <kimjungjin710@dgsw.hs.kr>
2026-06-01 12:32:44 +0900

    first commit

---
+++ src/main/java/com/example/kaidasubirserver/KaidasubirServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaidasubirserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaidasubirServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaidasubirServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kaidasubirserver/KaidasubirServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaidasubirserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaidasubirServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
